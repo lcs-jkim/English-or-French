45aa8fc28d82bc3193b98d75a1930bf5fade60d1
Amanda <jkim@lcs.on.ca>
2019-11-26 11:17:18 -0500

    Did one guard statement

--- English or French/ViewController.swift
+++ English or French/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: Properties
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var ErrorMessage: UILabel!
+    @IBOutlet weak var AnswerBox: UILabel!
+    
+    @IBAction func Analyze(_ sender: Any) {
+        
+        guard let TextFieldInput = TextField.text, TextFieldInput.count > 0 else {
+            ErrorMessage.text = ("Please enter your text")
+            return
+        }
+        
+    }
+    
 }
 
